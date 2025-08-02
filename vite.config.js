96d5e3f52bfb2ba8dbff3aa99c13344b624a1448
Anshumaan-25 <Anshumaan.singh0099@gmail.com>
2025-08-02 20:04:46 +0530

    fixed the bg problem probably

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Girlfriends-Day/', // 👈 This is the important line for GitHub Pages
+  base: '/Girlfriends-Day/', 
 })
